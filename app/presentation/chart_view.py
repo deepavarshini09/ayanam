08720716050960c1ad42d53cceb72025d068e94b
deepavarshini09 <deepavarshinisenkathir@gmail.com>
2026-06-14 23:41:41 +0200

    some changes

--- app/presentation/chart_view.py
+++ app/presentation/chart_view.py
@@ -3,15 +3,13 @@ from app.data.house_meanings import HOUSE_MEANINGS
 
 def build_house_chart(planet_positions, lagna_raasi, get_raasi_from_longitude, get_house_number):
 
-    house_map = {}
+    # Initialize all 12 houses as empty
+    house_map = {h: [] for h in range(1, 13)}
 
     for planet, lon in planet_positions.items():
 
         planet_raasi = get_raasi_from_longitude(lon)
         house = get_house_number(lagna_raasi, planet_raasi)
-
-        if house not in house_map:
-            house_map[house] = []
 
         house_map[house].append((planet, planet_raasi))
 
